e85dc4ada957f48318214626f38c7ad789906cc1
Aidana <108028245+aidanka01@users.noreply.github.com>
2022-10-17 18:28:17 +0300

    init commit

--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -1,13 +1,29 @@
 package peaksoft.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
+@ToString
 @Component
 public class AnimalsCage {
 
-    @Autowired
+
     private Animal animal;
+    private Timer timer;
+
+
+    public AnimalsCage(@Autowired @Qualifier("dog") Animal animal,
+                       @Autowired @Qualifier("timer") Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
